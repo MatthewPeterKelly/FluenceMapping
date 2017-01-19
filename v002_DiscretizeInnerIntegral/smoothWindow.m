a3c10d682f1d970603e489bf63acb76c9a4fd0c6
Matt Kelly <mkelly@rethinkrobotics.com>
2017-01-19 16:58:31 -0500

    started working on smooth integral stuff

--- v002_DiscretizeInnerIntegral/smoothWindow.m
+++ v002_DiscretizeInnerIntegral/smoothWindow.m
@@ -1,5 +1,5 @@
-function z = smoothWindow(xLow, x, xUpp, alpha)
-% z = smoothWindow(xLow, x, xUpp, alpha)
+function [z, zBool] = smoothWindow(xLow, x, xUpp, alpha)
+% [z, zBool] = smoothWindow(xLow, x, xUpp, alpha)
 %
 % This function is a smooth approximation to the boolean logic:
 %           z = (xLow < x) & (xUpp > x)
@@ -17,6 +17,31 @@ function z = smoothWindow(xLow, x, xUpp, alpha)
 %   z = [n1, n2] = smoothed output to boolean, on range [0,1]
 %
 
-% TODO:
+if nargin == 0
+    smoothWindow_test();
+    return;
+end
+
+zBool = zeros(size(x));
+zBool(x>xLow & x<xUpp) = 1;
+
+z = zBool;  % TODO:  smooth version of this!
+
+end
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+function smoothWindow_test()
+
+n = 100;
+xLow = -0.5*ones(1,n);
+xUpp = 0.5*ones(1,n);
+x = linspace(-1,1,n);
+alpha = 0.2;
+[z, zBool] = smoothWindow(xLow, x, xUpp, alpha);
+
+figure(14); clf; hold on;
+plot(x,zBool,'--');
+plot(x,z);
 
 end
